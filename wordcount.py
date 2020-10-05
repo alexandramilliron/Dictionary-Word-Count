09182527d22cc7a140943a1fc44f46a5c3ac6cb7
alexandramilliron <alexandramilliron@gmail.com>
2020-10-05 14:38:38 -0700

    completed function count_words and sys.arg setup

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,27 @@
-# put your code here.
+import sys
+
+def count_words(file):
+    """ Returns the count of all words in provided file argument. 
+
+    Input is a text file.
+    Output prints the words with their respective counts to the terminal.
+
+    """
+
+    count_words = {}
+
+    open_file = open(file)
+    
+    for line in open_file:
+        split_line = line.rstrip().lower().split(' ')
+        for word in split_line:
+            stripped_word = word.rstrip(",.?")
+            count_words[stripped_word] = count_words.get(stripped_word, 0) + 1
+    for word, count in count_words.items():
+        print(word, count)
+
+count_words(sys.argv[1])
+
+# if __name__ == '__main__':
+#     import doctest
+#     doctest.testmod()
