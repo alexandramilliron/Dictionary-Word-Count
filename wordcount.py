92d605bc823a17a21abbbfc75fc167efc74c2125
alexandramilliron <alexandramilliron@gmail.com>
2020-10-05 20:44:20 -0700

    commented out solution with counter and fixed .strip()

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 
 def count_words(file):
     """ Returns the count of all words in provided file argument. 
@@ -10,17 +11,27 @@ def count_words(file):
 
     count_words = {}
 
+    #cnt = Counter()
+
     open_file = open(file)
     
     for line in open_file:
-        split_line = line.rstrip().lower().split(' ')
+        split_line = line.rstrip().lower().split(" ")
         for word in split_line:
-            stripped_word = word.rstrip(",.?")
+            stripped_word = word.strip(",.?")
             count_words[stripped_word] = count_words.get(stripped_word, 0) + 1
     for word, count in count_words.items():
         print(word, count)
 
-count_words(sys.argv[1])
+    
+    # for word in stripped_list:
+    #     cnt[word] += 1
+
+    # return cnt 
+
+print(count_words(sys.argv[1]))
+
+
 
 # if __name__ == '__main__':
 #     import doctest
